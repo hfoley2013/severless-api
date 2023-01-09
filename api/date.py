2012dda8b7674a1206c0e522aec08d763f6f93bb
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 12:00:56 -0800

    m

--- api/date.py
+++ api/date.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_get(self):
+  def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
