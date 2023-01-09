3a55dc097fe4d627a0c867f5eed25c6b1cf62199
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 14:10:56 -0800

    requests only

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,36 +1,19 @@
-import http.server
 import requests
 
-class CapitalFinderHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Parse the query string
-        query_params = parse_qs(urlparse(self.path).query)
-
-        if "country" in query_params:
-            # Make a GET request to the REST Countries API with the given country name
-            response = requests.get(f"https://restcountries.eu/v2/name/{query_params['country'][0]}")
-            data = response.json()
-            # Return the capital of the country
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(f"The capital of {query_params['country'][0]} is {data[0]['capital']}.".encode())
-        
-        elif "capital" in query_params:
-            # Make a GET request to the REST Countries API with the given capital name
-            response = requests.get(f"https://restcountries.eu/v2/capital/{query_params['capital'][0]}")
-            data = response.json()
-            # Return the name of the country with the given capital
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(f"{query_params['capital'][0]} is the capital of {data[0]['name']['common']}.".encode())
-        
-        else:
-            self.send_response(400)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write("Please provide a country or capital name.".encode())
-
-def handler(req):
-    CapitalFinderHandler(req.headers, req.write, req.finish)
+def find_capital(request):
+    # Parse the query string
+    query_params = request.args
+    if "country" in query_params:
+        # Make a GET request to the REST Countries API with the given country name
+        response = requests.get(f"https://restcountries.eu/v2/name/{query_params['country']}?fullText=true")
+        data = response.json()
+        # Return the capital of the country
+        return f"The capital of {query_params['country']} is {data[0]['capital']}.", 200
+    elif "capital" in query_params:
+        # Make a GET request to the REST Countries API with the given capital name
+        response = requests.get(f"https://restcountries.eu/v2/capital/{query_params['capital']}")
+        data = response.json()
+        # Return the name of the country with the given capital
+        return f"{query_params['capital']} is the capital of {data[0]['name']['common']}.", 200
+    else:
+        return "Please provide a country or capital name.", 400
