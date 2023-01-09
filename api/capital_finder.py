6a6edd9078ec227c9d4d9884c0f31d45800d031e
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 15:42:38 -0800

    Fixes error handling

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -9,29 +9,29 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        if "country" in dic:
-            url = "https://restcountries.com/v3.1/name/"
-            r = requests.get(url + dic["country"])
-            data = r.json()
-            country = dic['country']
-            capital = str(data[0]['capital'][0])
-            message = f"The capital of {country.capitalize()} is {capital.capitalize()}"
-
-        elif "capital" in dic:
-            url = "https://restcountries.com/v3.1/capital/"
-            r = requests.get(url + dic['capital'])
-            data = r.json
-            capital = dic['capital']
-            country = str(data[0]['name']['common'][0])
-            message = f"The capital of {country.capitalize()} is {capital.capitalize()}."
-        
-        else:
+        if "country" or "capital" not in dic:
             message = "Give me a country or a capital to find."
 
+        else:
+
+            if "country" in dic:
+                query_country = dic["country"]
+                url = "https://restcountries.com/v3.1/name/"
+                r = requests.get(url + query_country.lower())
+            
+            if "capital" in dic:
+                query_capital = dic['capital']
+                url = "https://restcountries.com/v3.1/capital/"
+                r = requests.get(url + query_capital.lower())
+            
+            data = r.json()
+            country = str(data[0]['name']['common'])
+            capital = str(data[0]['capital'][0])
+            message = f"The capital of {country.capitalize()} is {capital.capitalize()}."
+        
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-
         self.wfile.write(message.encode())
 
         return
