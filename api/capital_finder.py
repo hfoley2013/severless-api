a19dfbe780cd0cd13261741a71b4df1f5bb5f3d5
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 13:54:28 -0800

    adds fastapi

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,35 +1,21 @@
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
 import requests
+from fastapi import FastAPI
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
-        print(query_string_list)
-        
-        if "capital" in dic:
-            url = "https://restcountries.com/v2/capital/"
-            r = requests.get(url + dic["capital"])
-            data = r.json()
-            definitions = []
-            for country_info in data:
-                definition = country_info["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = str(definitions)
+app = FastAPI()
 
-        else:
-            message = "Give me a country and I will return the capital city, or give me a capital city and I will return its country."
-
-        self.send_response(200)
-        self.send_header('Content-Type','application/json')
-        self.end_headers()
-
-        self.wfile.write(message.encode())
-
-        return
-
-# if __name__ == '__main__':
-#     handler(BaseHTTPRequestHandler)
+@app.get("/capital-finder")
+def find_capital(country: str = None, capital: str = None):
+    if country:
+        # Make a GET request to the REST Countries API with the given country name
+        response = requests.get(f"https://restcountries.eu/v2/name/{country}?fullText=true")
+        data = response.json()
+        # Return the capital of the country
+        return f"The capital of {country} is {data[0]['capital']}."
+    elif capital:
+        # Make a GET request to the REST Countries API with the given capital name
+        response = requests.get(f"https://restcountries.eu/v2/capital/{capital}")
+        data = response.json()
+        # Return the name of the country with the given capital
+        return f"{capital} is the capital of {data[0]['name']['common']}."
+    else:
+        return "Please provide a country or capital name."
