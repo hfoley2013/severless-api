c7778e41e2e707957d927c5e406439578fe26962
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 14:31:15 -0800

    simplifies http.server query

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -10,25 +10,13 @@ class handler(BaseHTTPRequestHandler):
         dic = dict(query_string_list)
 
         if "country" in dic:
-            response = requests.get(f"https://restcountries.com/v3.1/name/{dic['country']}")
-            data = response.json()
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
+            data = r.json()
+            message = f"The capital of {dic['country']} is {data[0]['capital']}"
 
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(f"The capital of {dic['country']} is {data[0]['capital']}.")
-        # if "word" in dic:
-        #     url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-        #     r = requests.get(url + dic["word"])
-        #     data = r.json()
-        #     definitions = []
-        #     for word_data in data:
-        #         definition = word_data["meanings"][0]["definitions"][0]["definition"]
-        #         definitions.append(definition)
-        #     message = str(definitions)
-
-        # else:
-        #     message = "Give me a word to define please"
+        else:
+            message = "Give me a country or a capital to find."
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
