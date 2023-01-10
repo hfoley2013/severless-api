16e36d6aa40054bcdae28fd5710a3a7b4ab825fb
Harper Foley <harper.e.foley@gmail.com>
2023-01-09 16:27:11 -0800

    Completes testing for deployment

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -11,24 +11,25 @@ class handler(BaseHTTPRequestHandler):
 
         if "country" or "capital" not in dic:
             message = "Give me a country or a capital to find."
-
-        else:
-
-            if "country" in dic:
-                query_country = dic["country"]
-                url = "https://restcountries.com/v3.1/name/"
-                r = requests.get(url + query_country.lower())
-            
-            if "capital" in dic:
-                query_capital = dic['capital']
-                url = "https://restcountries.com/v3.1/capital/"
-                r = requests.get(url + query_capital.lower())
-            
+                
+        if "country" in dic:
+            query_country = dic["country"]
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + query_country.lower())
             data = r.json()
             country = str(data[0]['name']['common'])
             capital = str(data[0]['capital'][0])
             message = f"The capital of {country.capitalize()} is {capital.capitalize()}."
-        
+
+        if "capital" in dic:
+            query_capital = dic['capital']
+            url = "https://restcountries.com/v3.1/capital/"
+            r = requests.get(url + query_capital.lower())
+            data = r.json()
+            country = str(data[0]['name']['common'])
+            capital = str(data[0]['capital'][0])
+            message = f"{capital.capitalize()} is the capital of {country.capitalize()}."
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
--- tests/test_capital_finder.py
+++ tests/test_capital_finder.py
@@ -1,29 +1,32 @@
 import pytest
-from "../api/capital_finder.py" import *
+import requests
+from http.server import HTTPServer
+from threading import Thread
+from api.capital_finder import handler
 
 def test_find_capital_by_country():
     # Test the handler class with a country name
     with HTTPServer(("", 8000), handler) as httpd:
         thread = Thread(target=httpd.serve_forever)
         thread.start()
-        response = requests.get("http://localhost:8000/capital-finder?country=Chile")
+        response = requests.get("http://localhost:8000/api/capital_finder?country=Chile")
         assert response.text == "The capital of Chile is Santiago."
         httpd.shutdown()
 
 def test_find_capital_by_capital():
-    # Test the handler class with a capital name
-    with HTTPServer(("", 8000), handler) as httpd:
+    # Test the handler(BaseHTTPRequestHandler) class with a capital name
+    with HTTPServer(("", 8001), handler) as httpd:
         thread = Thread(target=httpd.serve_forever)
         thread.start()
-        response = requests.get("http://localhost:8000/capital-finder?capital=Santiago")
-        assert response.text == "The capital of Chile is Santiago."
+        response = requests.get("http://localhost:8001/api/capital_finder?capital=Santiago")
+        assert response.text == "Santiago is the capital of Chile."
         httpd.shutdown()
 
 def test_find_capital_missing_parameter():
     # Test the handler class with no parameters
-    with HTTPServer(("", 8000), handler) as httpd:
+    with HTTPServer(("", 8002), handler) as httpd:
         thread = Thread(target=httpd.serve_forever)
         thread.start()
-        response = requests.get("http://localhost:8000/capital-finder")
+        response = requests.get("http://localhost:8002/api/capital_finder")
         assert response.text == "Give me a country or a capital to find."
         httpd.shutdown()
